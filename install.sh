9251d9dc58a8ceb93b9b8a94e9a99093279639d5
cdnss <32522595+cdnss@users.noreply.github.com>
2024-04-19 01:29:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone https://git.mollomm1.dev/Mollomm1/DesktopOnCodespaces
-cd DesktopOnCodespaces
+#git clone https://git.mollomm1.dev/Mollomm1/DesktopOnCodespaces
+#cd DesktopOnCodespaces
 pip install textual
 sleep 2
 python3 installer.py
